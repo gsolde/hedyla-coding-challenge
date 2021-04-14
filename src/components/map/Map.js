bb61667088b7c3c03245c3ba120b2a57024264cf
gsolde <gsolde@gmail.com>
2021-04-15 00:59:18 +0200

    feat: osrm route calculation integration with places. Still needs optimization & refactoring

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { GoogleMap, LoadScript, StandaloneSearchBox } from "@react-google-maps/api";
 import mapConfig from "../../mapConfig/mapConfig.json";
+import getRoutes from "../../services/osrm";
 import "./Map.css";
 
 export function Map() {
   const [originSearchBox, setOriginSearchBox] = useState({});
   const [destinationSearchBox, setDestinationSearchBox] = useState({});
+  let origin, destination;
 
   function onLoadOrigin(ref) {
     setOriginSearchBox(ref);
@@ -18,11 +20,25 @@ export function Map() {
   }
 
   function onPlacesChangedOrigin() {
-    console.log(originSearchBox.getPlaces());
+    origin = {
+      name: originSearchBox.getPlaces()[0].formatted_address,
+      lat: originSearchBox.getPlaces()[0].geometry.location.lat(),
+      lng: originSearchBox.getPlaces()[0].geometry.location.lng(),
+    };
+    console.log(origin);
   }
 
   function onPlacesChangedDestination() {
-    console.log(destinationSearchBox.getPlaces());
+    destination = {
+      name: destinationSearchBox.getPlaces()[0].formatted_address,
+      lat: destinationSearchBox.getPlaces()[0].geometry.location.lat(),
+      lng: destinationSearchBox.getPlaces()[0].geometry.location.lng(),
+    };
+    console.log(destination);
+  }
+
+  function fetchRoute(org, dest) {
+    getRoutes(org, dest);
   }
 
   return (
@@ -41,6 +57,7 @@ export function Map() {
             <StandaloneSearchBox onLoad={onLoadDestination} onPlacesChanged={onPlacesChangedDestination}>
               <input className="searchBox" type="text" placeholder="Destination" />
             </StandaloneSearchBox>
+            <button onClick={() => fetchRoute(origin, destination)}>Get it!</button>
           </div>
         </GoogleMap>
       </LoadScript>
--- src/services/osrm.js
+++ src/services/osrm.js
@@ -1,6 +1,6 @@
-export function getRoutes() {
+export default function getRoutes(org, dest) {
   fetch(
-    `http://router.project-osrm.org/route/v1/driving/2.0088747,41.563211;2.2459643,41.4473147?alternatives=true&geometries=polyline&steps=true`
+    `http://router.project-osrm.org/route/v1/driving/${org.lng},${org.lat};${dest.lng},${dest.lat}?alternatives=true&geometries=polyline&steps=true`
   )
     .then((res) => res.json())
     .then((data) => console.log(data))
