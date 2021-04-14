b38d72dec78934b18a2f8aac44bb533b383994d7
gsolde <gsolde@gmail.com>
2021-04-14 19:41:36 +0200

    chore: first commit, set folder and file structure

--- src/features/routeCalculator/routeCalculatorSlice.js
+++ src/features/routeCalculator/routeCalculatorSlice.js
@@ -1,9 +1,9 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getRoutes } from "../../services/osrm";
 
 const initialState = {
   value: 0,
-  status: 'idle',
+  status: "idle",
 };
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -11,17 +11,14 @@ const initialState = {
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
-  }
-);
+export const incrementAsync = createAsyncThunk("counter/getRoutes", async () => {
+  const response = await getRoutes();
+  // The value we return becomes the `fulfilled` action payload
+  return response.data;
+});
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const routeCalculatorSlice = createSlice({
+  name: "route calculator",
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
@@ -45,29 +42,29 @@ export const counterSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(incrementAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(incrementAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.value += action.payload;
       });
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = routeCalculatorSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectRoute = (state) => state.routeCalculator.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
 export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
+  const currentValue = selectRoute(getState());
   if (currentValue % 2 === 1) {
     dispatch(incrementByAmount(amount));
   }
 };
 
-export default counterSlice.reducer;
+export default routeCalculatorSlice.reducer;
