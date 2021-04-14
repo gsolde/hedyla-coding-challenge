a9eba72242e9e9c9606ca45279937d98a0b8eca8
gsolde <gsolde@gmail.com>
2021-04-14 20:42:58 +0200

    feat: file and components cleanup

---
+++ src/components/routeCalculator/RouteCalculator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./RouteCalculator.css";
+
+export function RouteCalculator() {
+  return (
+    <div>
+      <div className="row">
+        <input className="textbox" aria-label="Set increment amount" />
+        <button className="button">Calculate!</button>
+      </div>
+      <h1></h1>
+    </div>
+  );
+}
