2351babee26f91570a5c40e2657cc90f58448e0a
gsolde <gsolde@gmail.com>
2021-04-17 21:00:21 +0200

    chore: delete services folder

--- src/services/osrm.js
+++
@@ -1,8 +0,0 @@
-export default function getRoutes(org, dest) {
-  fetch(
-    `http://router.project-osrm.org/route/v1/driving/${org.lng},${org.lat};${dest.lng},${dest.lat}?alternatives=true&geometries=polyline&steps=true`
-  )
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((error) => console.log(error));
-}
